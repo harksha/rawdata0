8b2fc3a57eb9a012ce24e4d23924f521ce5c6c8b
Ioana <peacelovetheysay@gmail.com>
2015-12-20 12:55:42 +0100

    config change

--- portfolio2gr4/Scripts/require.config.js
+++ portfolio2gr4/Scripts/require.config.js
@@ -1,11 +1,19 @@
 ﻿require.config({
-  baseUrl: "/",
-  paths: {
-    jquery: "Scripts/jquery-2.1.4.min",
-    knockout: "Scripts/knockout-3.4.0",
-    bootstrap: "Scripts/bootstrap.min"
-  },
-  shim: { 
-    'bootstrap': ['jquery']
-  }
+	baseUrl: "/",
+	paths: {
+		jquery: "Scripts/jquery-2.1.4.min",
+		knockout: "Scripts/knockout-3.4.0",
+		bootstrap: "Scripts/bootstrap.min"
+	}
+	,
+	shim: { 
+  	"bootstrap": {
+  		deps: ["jquery"],
+  		exports: "$.fn.popover"
+  	},
+	enforceDefine: true
+	}
+});
+require(['jquery', 'bootstrap'], function ($) {
+	return {};
 });
